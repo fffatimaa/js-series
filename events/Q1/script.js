0372622f5dec98fb8a83971d427d7dbc424a2daa
Fatima Faisal <fatimaafaisal44@gmail.com>
2025-09-01 21:29:09 +0500

    events

---
+++ events/Q1/script.js
@@ -0,0 +1,21 @@
+//create toggle btn changes the screen color light nd dark mode
+
+let modebtn = document.querySelector('#mode');
+let body = document.querySelector("body");
+let currMode ="light";
+
+modebtn.addEventListener("click", ()=>{
+
+if(currMode ==="light" ){
+    currMode="dark"
+    body.classList.add("dark");
+    body.classList.remove("light");
+}
+else {
+    currMode="light"
+    body.classList.add("light");
+    body.classList.remove("dark");
+}
+console.log(currMode);
+
+});
