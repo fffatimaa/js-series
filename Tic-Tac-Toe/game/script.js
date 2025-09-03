c741800ce0af5a4cf79b5eb2eca9425802707980
Fatima Faisal <fatimaafaisal44@gmail.com>
2025-09-03 21:31:18 +0500

    done

--- Tic-Tac-Toe/game/script.js
+++ Tic-Tac-Toe/game/script.js
@@ -1,5 +1,8 @@
-let boxes =document.querySelectorAll(".box");
+let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true; 
 
@@ -32,5 +35,44 @@ boxes.forEach((box) => {
        }
        box.disabled = true;
        //can't be clicked again
-    })
-})
+
+       checkWinner();
+    });
+});
+    const checkWinner = () =>{
+        for (let pattern of winPatterns){
+            let pos1Val = boxes[pattern[0]].innerText;
+            let pos2Val = boxes[pattern[1]].innerText;
+            let pos3Val = boxes[pattern[2]].innerText;
+        if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner", pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+        }
+    };
+    const showWinner = (winner) => {
+        msg.innerText= `Congratualations, Winner is ${winner}`;
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+
+    }
+    const disableBoxes =() =>{
+        for(let box of boxes){
+            box.disabled = true;
+        }
+    };
+    const enableBoxes =() =>{
+        for(let box of boxes){
+            box.disabled = false;
+            box.innerText ="";
+        }
+    };
+    const resetGame =() =>{
+        turnO= true;
+        enableBoxes();
+        msgContainer.classList.add("hide");
+    };
+    newGameBtn.addEventListener("click" , resetGame);
+    resetBtn.addEventListener("click" , resetGame);
