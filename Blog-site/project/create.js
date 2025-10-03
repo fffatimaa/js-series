598e0e00d408d8b3891dc6603e50c63fc7c6f2ea
Fatima Faisal <fatimaafaisal44@gmail.com>
2025-10-04 00:05:33 +0500

    js part

---
+++ Blog-site/project/create.js
@@ -0,0 +1,25 @@
+class blog{
+    constructor(image , title, body, author){
+        this.image = image;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+        this.date = new Date().toLocaleDateString();
+    }
+}
+let form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  let image = document.getElementById("image").value;
+  let title = document.getElementById("title").value;
+  let body = document.getElementById("body").value;
+  let author = document.getElementById("author").value;
+  if(image==="" || title==="" || body==="" || author===""){
+    alert(" fill all");
+    return;
+  }
+ 
+});
+
